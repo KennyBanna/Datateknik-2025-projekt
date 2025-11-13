9a1f190593732526090b3ed1389bec9ab3abfd2c
KennyBanna <gravadlaxo93@hotmail.com>
2025-11-13 19:19:25 +0100

    Added quiz page, boilerplate js for the quiz. Quiz questions can be added to quiz.json

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,13 +1,44 @@
-const correctAnswers = {
-    1: "a",
-    2: "b",
-    3: "a",
-};
+document.addEventListener("DOMContentLoaded", () => {
+    
+    var questionIndex = 0;
+    var questionData = null;
 
-function isCorrect(question_id, answer) {
-    if (answer === correctAnswers[question_id]) {
-        return true;
-    } else {
-        return false;
+    const questionLabel = document.getElementById("question-label");
+    const questionNumberDisplay = document.getElementById("question-index-span");
+    const button = document.getElementById("answer-button");
+    button.addEventListener(("click"), () => nextQuestion());
+
+    // Function to load all questions in to an object from json file
+    async function loadQuestionsFromJsonFile() {
+        const response = await fetch('/quiz/quizQuestions.json');
+        const questionData = await response.json();
+        return questionData;
+    } 
+    
+    async function displayQuestion(){
+
+        if(questionData == null){
+            questionData = await loadQuestionsFromJsonFile();
+        }
+
+        const question = questionData[questionIndex].question;
+        const options = questionData[questionIndex].options;
+
+        questionLabel.innerHTML = question;
+        questionNumberDisplay.innerHTML = questionIndex + 1;
+        
+        for(key in options){
+            var element = document.getElementById(`label-${key}`);
+            element.innerHTML = key + ": " + options[key];
+        }
     }
-}
+
+    function nextQuestion(){
+
+        questionIndex ++
+
+        displayQuestion();
+    }
+
+    displayQuestion();
+});
