d7312a900ef1421c29dff66043a6a0cb63ce4706
KennyBanna <gravadlaxo93@hotmail.com>
2025-11-21 22:04:31 +0100

    Lade till ett slut på quizzen där man får se hur många rätt man hade. Basic som stryk

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -2,16 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
     
     var questionIndex = 0;
     var questionData = null;
+    var userAnwers = [];
+    var amountOfQuestions;
 
     const questionLabel = document.getElementById("question-label");
     const questionNumberDisplay = document.getElementById("question-index-span");
     const button = document.getElementById("answer-button");
-    button.addEventListener(("click"), () => nextQuestion());
+    button.addEventListener(("click"), () => answerQuestion());
 
     // Function to load all questions in to an object from json file
     async function loadQuestionsFromJsonFile() {
         const response = await fetch('/quiz/quizQuestions.json');
         const questionData = await response.json();
+
+        amountOfQuestions = await questionData.length;
+
         return questionData;
     } 
     
@@ -33,11 +38,43 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    function nextQuestion(){
+    function answerQuestion(){
 
+        var radionButtons = document.getElementsByName("answer");
+        var answer = '';
+        for(i = 0; i < 4; i++){
+            if(radionButtons[i].checked){
+                answer = radionButtons[i].value;
+                break;
+            }   
+        }
+        
+        userAnwers.push(answer);
         questionIndex ++
 
+        //If its the final questions - Display result and bring user back to home
+        if(questionIndex == amountOfQuestions){
+            correctQuiz();
+            return;
+        }
+
         displayQuestion();
+    }
+
+    function correctQuiz(){
+
+        var correctAnswers = 0;
+
+        for(i = 0; i < amountOfQuestions; i++){
+
+            if(userAnwers[i] == questionData[i].correctAnswer){
+                correctAnswers ++;
+            }
+
+        }
+
+        alert("You had " + correctAnswers + " out of " + amountOfQuestions + " correct answers");
+        open("index.html")
     }
 
     displayQuestion();
